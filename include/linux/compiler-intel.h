be9697f6bf689c26c911c6bc00d6c0e50ec3c774
Pranith Kumar <bobby.prani@gmail.com>
2014-04-15 12:05:22 -0400

    compiler-intel.h: Remove duplicate definition
    barrier is already defined as __memory_barrier in compiler.h
    Remove this unnecessary redefinition.
    
    Signed-off-by: Pranith Kumar <bobby.prani@gmail.com>
    Link: http://lkml.kernel.org/r/CAJhHMCAnYPy0%2BqD-1KBnJPLt3XgAjdR12j%2BySSnPgmZcpbE7HQ@mail.gmail.com
    Signed-off-by: H. Peter Anvin <hpa@linux.intel.com>
    Signed-off-by: Albert I <krascgq@outlook.co.id>

--- include/linux/compiler-intel.h
+++ include/linux/compiler-intel.h
@@ -13,10 +13,7 @@
 /* Intel ECC compiler doesn't support gcc specific asm stmts.
  * It uses intrinsics to do the equivalent things.
  */
-#undef barrier
 #undef RELOC_HIDE
-
-#define barrier() __memory_barrier()
 
 #define RELOC_HIDE(ptr, off)					\
   ({ unsigned long __ptr;					\
