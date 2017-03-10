AshishM94/Kenzo

diag: Fix GCC 6 tautological-compare warning

drivers/char/diag/diagfwd.c
@@ -411,7 +411,7 @@ void diag_update_pkt_buffer(unsigned char *buf, uint32_t len, int type)
 	}
 
 	mutex_lock(&driver->diagchar_mutex);
-	if (CHK_OVERFLOW(ptr, ptr, ptr + max_len, len)) {
+	if (CHK_OVERFLOW(ptr, ptr + max_len, len)) {
 		memcpy(ptr, temp , len);
 		*length = len;
 		*in_busy = 1;

aphisitworachorch/kiwi2

Signed-off-by: Albert I <krascgq@outlook.co.id>

drivers/char/diag/diagfwd.c
@@ -1120,7 +1120,7 @@ void diag_update_pkt_buffer(unsigned char *buf, int type)
 		return;
 	}
 	mutex_lock(&driver->diagchar_mutex);
-	if (CHK_OVERFLOW(ptr, ptr, ptr + PKT_SIZE, length)) {
+	if (CHK_OVERFLOW(ptr, ptr + PKT_SIZE, length)) {
 		memcpy(ptr, temp , length);
 		*in_busy = 1;
 	} else {